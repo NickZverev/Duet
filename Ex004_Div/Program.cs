dfb8a37346c6f1f3e40e60332f756661d06e12fd
Nikolai <ZverevNA12345@outlook.com>
2022-09-25 01:20:07 +0300

    add Ex004_Div

---
+++ Ex004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу, в которую пользователь вводит 
+// два целых числа и получает частное введённых чисел. 
+double namberA = 18;
+double namberB = 5;
+Console.WriteLine(namberA / namberB);
